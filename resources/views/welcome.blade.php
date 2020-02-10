b4f8fd2c723ddcabdb4d17dd75f3cca6d4a18ec3
vagrant <vagrant@homestead>
2020-02-10 05:01:54 +0000

    vueへの変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,11 +84,17 @@
                     Laravel
                 </div>
 
-                <div id="example"></div>
-                <script src="{{mix('js/app.js')}}"></script>
-
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
             </div>
         </div>
-
     </body>
 </html>
